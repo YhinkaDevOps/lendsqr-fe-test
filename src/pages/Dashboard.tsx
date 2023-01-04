bedcdcf83b9278252c6bf36a6c0de6c6c901da3b
YhinkaDevOps <yinkabakare30@gnail.com>
2023-01-04 14:01:10 +0200

    Dashboard header

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,13 @@
+import AppLayout from 'components/Layout/Layout'
+
+const Dashboard = () => {
+  return (
+    <>
+      <AppLayout>
+        <h1 className="dashboard__header">Welcome to Dashboard</h1>
+      </AppLayout>
+    </>
+  )
+}
+
+export default Dashboard
